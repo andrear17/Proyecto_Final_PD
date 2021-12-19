c80028e876fd374c025e3b39b23b2369a32e6a0c
andrear17 <andrea.rivas@galileo.edu>
2021-12-19 01:29:47 -0600

    Update pipeline_predict.py

--- pipeline_predict.py
+++ pipeline_predict.py
@@ -4,13 +4,10 @@ import numpy as np
 
 #Cargamos modelo y pipeline
 kidney_disease_model = joblib.load('kidney_disease_pipeline.pkl')
-X_test = pd.read_csv('X_test.csv')
 
 #Funcion para hacer predicciones.
 def predict(X):
     predicts = kidney_disease_model.predict(X)
+    target_mapping = {0:'notckd', 1:'ckd'}
+    predicts = pd.Series(predicts).map(target_mapping)
     return predicts[0]
-
-#print(predict(X_test))
-#X_test = pd.json_normalize({"PassengerId":pd.NA,"Pclass":1,"Name": "Mike Dallas","Sex": "male","Age": 22,"SibSp": 0,"Parch": 0,"Ticket": 649,"Fare": 7.25,"Cabin": "B14","Embarked":"S"})
-#print('Resultado', predict(X_test))
